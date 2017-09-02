5bea658fa2169db5f141224e9e61158a55842902
guillebecker15@gmail.com <guille.becker.15@gmail.com>
2017-09-02 18:56:12 -0300

    ruta login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 	resources :usuarios
 	root "debugger#index"
+	get '/login', to: 'usuarios#login', as: 'login'
 	
 	get 'debugger/index'
 
-	get '/canales', to: 'canales#index', as: 'canales'
+	get '/canales/{nikcname}/{password}', to: 'canales#index', as: 'canales'
 	#get '/usuarios', to: 'usuarios#index', as: 'usuarios'
+
 
 end
