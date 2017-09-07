5893951179581c1ee7cb647d5bd0c733fb258157
Carlos Frostte <carlosfrostte@gmail.com>
2017-09-07 09:34:47 -0300

    Usar authenticate_user_from_token en vez de authenticate_usuario en ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,27 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-	before_action :authenticate_usuario!
-	respond_to :json
-	# before_action :authenticate_usuario!, except: [:index, :home]
-
-	# before_action :authenticate_user_from_token!
-
-  # Enter the normal Devise authentication path,
-  # using the token authenticated user if available
-  # before_filter :authenticate_usuario!
+  
+  respond_to :json # Soporte para peticiones JSON (excepto GET y HEAD)
+  
+  protect_from_forgery with: :exception # Proteccion de solicitudes falsas
+  
+  # before_action :authenticate_usuario!, except: [:index, :home]
+	before_action :authenticate_user_from_token!
 
   private
 
   def authenticate_user_from_token!
+  
     authenticate_with_http_token do |token, options|
+  
       user_email = options[:email].presence
       user = user_email && Usuario.find_by_email(user_email)
 
       if user && Devise.secure_compare(user.authentication_token, token)
         sign_in user, store: false	
       end
+  
     end
+  
   end
+
 end
