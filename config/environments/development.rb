a04546d126a73ed72cb4aab884e0f9ab0427c89f
Carlos Frostte <carlosfrostte@gmail.com>
2017-09-12 20:39:15 -0300

    ...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,4 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.default_url_options = { :host => "http://localhost" }
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://redtiprails.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: 'redtiprails.herokuapp.com', port: 3000 }
 
 end
